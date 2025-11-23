66d72ff22a5a13ee7e8bf0ef9902830c05578536
bea-urland <beatrice.urland@icloud.com>
2025-11-23 21:31:04 +0100

    4.3 complete

--- module-4/task_4-3/task_4-3.mjs
+++ module-4/task_4-3/task_4-3.mjs
@@ -47,15 +47,82 @@ const MovieGenre = [
 
 //--- Part 1 ----------------------------------------------------------------------------------------------
 /* Put your code below here!*/
+function cmbTask1CalculaterClick(){
+  const txtTask1Output = document.getElementById("txtTask1Output");
+  const txtRectHeight = document.getElementById("txtRectHeight");
+  const txtRectWidth = document.getElementById("txtRectWidth");
+  const height = parseInt(txtRectHeight.value);
+  const width = parseInt(txtRectWidth.value);
+  const area = height * width;
+  const perimeter = 2 * (height + width);
+  txtTask1Output.innerHTML = `width: ${width}, height: ${height}`;
+  txtTask1Output.innerHTML += `<br/>area: ${area}, perimeter: ${perimeter}`;
+}
+
+let cmbTask1Calculate = document.getElementById("cmbTask1Calculate");
+cmbTask1Calculate.onclick = cmbTask1CalculaterClick;
 
 //--- Part 2 ----------------------------------------------------------------------------------------------
 /* Put your code below here!*/
+function txtTask2WordKeyPress (aEvent){
+  //txtTask2Output.innerHTML = 'You pressed: ${aEvent.key}';
+  if (aEvent.key === "Enter"){
+    const word = txtTask2Word.value;
+    task2Words.push(word);
+    txtTask2Output.innerHTML = `You have entered ${task2Words.length} words: <br/>`
+    txtTask2Output.innerHTML += txtTask2WordKeyPress.join(" ");
+    txtTask2Word.value = "";
+}
+} 
+const txtTask2Word = document.getElementById("txtTask2Word");
+txtTask2Word.addEventListener("keypress", txtTask2WordKeyPress);
+const task2Words = [];
 
 //--- Part 3 ----------------------------------------------------------------------------------------------
 /* Put your code below here!*/
+const chkTask3 = document.getElementsByName("chkTask3");
+const cmbTask3CheckAnswer = document.getElementById("cmbTask3CheckAnswer");
+const txtTask3Output = document.getElementById("txtTask3Output");
+
+function cmbTask3CheckAnswerClick(){
+  for(let i = 0; i < chkTask3.length; i++){
+    const chkBox = chkTask3[i];
+    const text = `chkTask3[${i}].checked = ${chkTask3[i].checked}`;
+    txtTask3Output.innerHTML += text + "<br/>";
+  
+  }
+}
+cmbTask3CheckAnswer.addEventListener("click", cmbTask3CheckAnswerClick);
 
 //--- Part 4 ----------------------------------------------------------------------------------------------
 /* Put your code below here!*/
+function rdbCarNameSelect(aEvent){
+  const txtTask40Output = document.getElementById("txtTask4Output");
+  txtTask40Output.innerHTML = 
+  `User selected car type number: ${aEvent.target.value}`
+  console.log(aEvent.target)
+}
+
+for(let i = 0; i < CarTypes.length; i++){
+  const car = CarTypes[i];
+  const inpRadio = document.createElement("input");
+  inpRadio.type = "radio";
+  inpRadio.name = "rdbCarName";
+  inpRadio.value = car.value;
+  const lblRadio = document.createElement("label");
+  inpRadio.id = "rdbCarName" + i.toString();
+  inpRadio.addEventListener("click", rdbCarNameSelect);
+
+  const lblCaption = document.createElement("label");
+  lblCaption.for = inpRadio.id;
+  lblCaption.appendChild(document.createTextNode(car.caption));
+  divTask4Cars.appendChild(inpRadio);
+  divTask4Cars.appendChild(lblCaption);
+  divTask4Cars.appendChild(document.createElement("br"));
+ 
+  console.log('CarTypes[${i}].value = ${car.value}, CarTypes[${i}].caption = ${car.caption}');
+
+}
 
 //--- Part 5 ----------------------------------------------------------------------------------------------
 /* Put your code below here!*/
